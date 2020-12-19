ba70690929ad152dd5681460803e3e89e2caab54
wilfriedndr <56036508+wilfriedndr@users.noreply.github.com>
2020-12-19 17:20:40 +0100

    Add files via upload

--- evaluation/project/pendu.py
+++ evaluation/project/pendu.py
@@ -1,18 +1,54 @@
-lives = 5
-solution = "python"
-Fletters = ""
-displayWord = ""
+import random
 
-for i in solution:
+listWords = []
+o = ""
+
+with open('words.txt') as text1:
+	line = text1.readline()
+
+	while line:
+		listWords.append(line)
+		line = text1.readline()
+
+for o in listWords:
+  o.split()
+
+solution = random.choice(listWords)
+lives = 5
+displayWord = ""
+Fletter = ""
+
+for l in solution:
   displayWord = displayWord + "_ "
 
-while lives>0:
-    print("Guess the word : ", displayWord)
-    guessLetter = input("Lettre ? ")
-    if guessLetter in solution:
-        Fletters = Fletters + guessLetter
-        print("Trouvé")
-    else:
-        lives -= 1
-        print("Raté ! ", lives, " chances.")
+  print(">>> Welcome to Hangman!\n>>> Activez les caps !")
+  print(solution)
+  
+
+
+while lives > 0:
+  print("\nMot à deviner : ", displayWord)
+  guessLetter = input(">>> Guess your letter : ")
+
+  if guessLetter in solution:
+    print(solution)
+    Fletter = Fletter + guessLetter
+    print("Vous avez trouvé la lettre")
+    
+  else:
+    lives -= 1
+    print("Incorrect! You have ", lives," guesses...")
+    print(solution)
+    
+
+  displayWord = ""
+  for x in solution:
+      if x in Fletter:
+          displayWord += x + " "
+      else:
+          displayWord += "_ "
+
+  if "_" not in displayWord:
+      print("Well done !")
+      break
 
